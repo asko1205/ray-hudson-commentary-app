a2869fa2e27e30bb04d0cffad100fee5d70603df
Alex Skodyn <alex@schoolzilla.com>
2017-07-09 21:37:29 -0700

    Playing around with expo audio sound

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Quote from './components/Quote';
 
+import Expo, { Asset, Audio, Font, Video } from 'expo';
+
+
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this._loadNewPlaybackInstance();
+  }
+
+  async _loadNewPlaybackInstance() {
+    if (this.playbackInstance != null) {
+      await this.playbackInstance.unloadAsync();
+      this.playbackInstance.setCallback(null);
+      this.playbackInstance = null;
+    }
+
+    const source = { uri: "https://s3-us-west-2.amazonaws.com/ray-hudson-soundbites/01matadorinhighheels.mp3" };
+    const sound = new Expo.Audio.Sound({
+      source: source.uri
+    });
+    this.playbackInstance = sound;
+  }
+
+  _onPlayPausePressed = () => {
+  console.log("hi there")
+   if (this.playbackInstance != null) {
+     this.playbackInstance.loadAsync();
+      this.playbackInstance.setPositionAsync(0)
+      this.playbackInstance.playAsync();
+   }
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Best of Ray Hudson: Football Quotes</Text>
-        <Quote></Quote>
+        <Button
+          onPress={this._onPlayPausePressed}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Quote />
       </View>
     );
   }
